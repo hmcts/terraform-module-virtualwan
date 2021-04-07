8b2f07a2a967b9a8d4a4c68871101fb750b9707c
TomKimber <thomas.kimber@hmcts.net>
2021-04-07 16:08:09 +0100

    Added additional outputs to 99-outputs.tf

--- 99-outputs.tf
+++ 99-outputs.tf
@@ -1,3 +1,11 @@
+output "resource_group_location" {
+  value = var.resource_group_name != null ? azurerm_resource_group.virtual_wan_resource_group[0].location : null
+}
+
+output "resource_group_name" {
+  value = var.resource_group_name != null ? azurerm_resource_group.virtual_wan_resource_group[0].name : null
+}
+
 output "virtual_hub_connection_ids" {
   value = tomap({
     for connection in azurerm_virtual_hub_connection.virtual_hub_connection : connection.name => connection.id
